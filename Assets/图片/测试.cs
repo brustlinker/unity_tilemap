2a7072272cb77d764b7ecc48397dc6cf77a03151
brustlinker <1634694819@qq.com>
2020-01-04 14:55:04 +0800

    第一次递交
    第一次递交

---
+++ Assets/图片/测试.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class 测试 : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase tilebase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //清空所有的格子
+        //tilemap.ClearAllTiles();
+        TileBase tile = tilemap.GetTile(new Vector3Int(-2, -2, 0));
+        Debug.Log(tile);
+
+
+        tilemap.SetTile(Vector3Int.zero,tilebase);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
